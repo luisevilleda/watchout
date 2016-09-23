83c873e52cf44b43d54f27c12283941289a586b6
luisevilleda <luisevilleda@gmail.com>
2016-09-23 14:24:07 -0700

    ADd working example

--- client/watchout.js
+++ client/watchout.js
@@ -1,1 +1,16 @@
 // start slingin' some d3 here.
+
+
+//create asteroids
+  //asteroids will be an array of sub-arry x, y coords within the board
+    //every time we want to move the asteroids, loop through the asteroids array
+      //generate a random x, y within the bounds of the board
+
+//track your mouse pointer
+  //store the mouse coords in an array [x, y]
+    // coordinates = d3.mouse(this);
+  //access x, y
+    //var x = coordinates[0];
+    //var y = coordinates[1];
+
+
